6d2ed591f95eacab7a8ca9b983944f708f91d4bf
= <diegomarcel27@hotmail.com>
2020-02-21 15:29:13 -0300

    Advancements in communication between server and client. Problems with reading lines to be solved.

--- lib/client.rb
+++ lib/client.rb
@@ -1,5 +1,15 @@
 require 'socket'  
-streamSock = TCPSocket.new( "127.0.0.1", 5000 )  
-str = streamSock.recv( 100 )  
-puts str  
-streamSock.close  
+streamSock = TCPSocket.new( "127.0.0.1", 5001 )  
+
+puts 'TYPE X TO EXIT, OTHERWISE TYPE MESSAGE:'
+message = gets
+while message != 'X'
+  streamSock.puts(message)
+  puts 'WAITING FOR RESPONSE'
+  server_message = streamSock.gets
+  print server_message
+  puts 'NEXT MESSAGE:'
+  message = gets
+end
+
+streamSock.close
--- lib/connection_handler.rb
+++ lib/connection_handler.rb
@@ -7,18 +7,34 @@ class ConnectionHandler
   end
   
   def handle_client(socket, closing_callback)
+    puts 'HANDLING CLIENT'
     while line = socket.gets
-      process_line(socket, line)
+      puts "the line: #{line}"
+      response = process_line(socket, line)
+      puts "RESPONSE: #{response}"
+      socket.puts(response)
+      puts 'WAITING FOR NEXT MESSAGE'
     end
   end
 
 private
 
   def process_line(socket, line)
-    return @executor.execute_command(line, method(:get_data))
+    parsed_command = @executor.split_arguments(line)
+    command_args = parsed_command.command_args
+    if @executor.is_storage?(command_args[:command])
+      puts 'Storage command, we need to fetch data'
+      socket.puts('SEND DATA')
+      data = get_data(socket, command_args[:bytes])
+      return @executor.execute_storage(command_args, data)
+    end
+    return @executor.execute_retrieval(command_args)
   end
 
   def get_data(socket, length)
-    return socket.recv(length)
+    puts length
+    data =  socket.recv(length + 2)
+    puts 'DATA RECEIVED'
+    return data
   end
 end
